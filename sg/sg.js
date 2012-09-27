677f7997d3951e0278650bf005d5c21a20587d48
Benjamin Horsleben <benjamin@fizkerinc.dk>
2012-09-28 00:52:15 +0200

    Got the images to show correctly. Got the images to slide in and out. Got keyboard navigation.

--- sg/sg.js
+++ sg/sg.js
@@ -1,8 +1,14 @@
-var _mainElement = _.createElement('<section class="root"></section>')
+var images = prepareImages(pics)
+  , _mainElement = _.createElement('<section class="root"></section>')
   , _loader = _.createElement('<progress value=0 max=' + pics.length + '></progress>')
   , _button = _.createElement('<button class="btn-enable-fullscreen">Go to fullscreen</button>')
 
 _mainElement.appendTo(document.body)
+
+images.forEach(function(img) {
+	img._elm.appendTo(_mainElement)
+})
+images[0].show()
 
 _.createElement('<div class="loading-views"></div>')
 	.append(_loader)
@@ -20,36 +26,52 @@ _button
 		_mainElement.requestFullScreen()
 	})
 
-var images = pics.map(function(pic, idx) {
-	var _img = _.createElement('<img src=' + pic[0] + '></img>')
-
-	_img
-		.on('load', function() {
-			_loader.obj.value++
+function prepareImages(pics) {
+	return pics
+		.filter(function(pic) {
+			return pic
 		})
-		.appendTo(_mainElement)
-		.addClass('next')
+		.map(function(pic, idx) {
+			var _img = _.createElement('<img data-idx=' + idx + ' src=' + pic[0] + '></img>')
+			    .addClass('next')
+			    .on('load', function() { _loader.obj.value++ })
 
-	_img.obj.dataset.idx = idx
+			return { _elm: _img
+			       , show: show
+			       }
 
-	return { _elm: _img
-	       , show: show
-	       }
+			function show() {
+				var current = $$('.current')
+				if(current) {
+					_(current)
+						.addClass(current.dataset.idx < idx ? 'prev' : 'next')
+						.removeClass('current')
+				}
+				_img
+					.addClass('current')
+					.removeClass('prev', 'next')
+			}
+		})
+}
 
-	function show() {
-		var current = $$('.current')
-		if(current) {
-			_(current)
-				.addClass(current.dataset.idx < idx ? 'prev' : 'next')
-				.removeClass('current')
-		}
-		_img
-			.addClass('current')
-			.removeClass('prev', 'next')
+function next() {
+	var current = $$('.current')
+	  , next = images[+current.dataset.idx+1]
+
+	if(!next) {
+		next = images[0]
 	}
-})
+	next.show()
+}
+function prev() {
+	var current = $$('.current')
+	  , prev = images[+current.dataset.idx-1]
 
-images[0].show()
+	if(!prev) {
+		prev = images[images.length-1]
+	}
+	prev.show()
+}
 
 window.addEventListener('keyup', function(event) {
 	switch(event.keyCode) {
